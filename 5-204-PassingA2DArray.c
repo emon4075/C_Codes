0b6452aa61b1a5a31a4a7098d76ebef1cc83273e
emon4075 <mdabdullahemon4075@gmail.com>
2023-08-20 11:33:54 +0600

    Function Update

--- 5-204-PassingA2DArray.c
+++ 5-204-PassingA2DArray.c
@@ -40,3 +40,45 @@ int main()
 
     return 0;
 }
+
+// Another Version with both input and output
+/*
+
+#include <stdio.h>
+void ScanArray(int row, int col, int array[row][col])
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("[%d] [%d] Index: ", i, j);
+            scanf("%d", &array[i][j]);
+        }
+        printf("\n");
+    }
+}
+void DisplayArray(int row, int col, int array[row][col])
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int row, col;
+    printf("Enter rows: ");
+    scanf("%d", &row);
+    printf("Enter cols: ");
+    scanf("%d", &col);
+    int array[row][col];
+    ScanArray(row, col, array);
+    DisplayArray(row, col, array);
+    return 0;
+}
+
+*/
